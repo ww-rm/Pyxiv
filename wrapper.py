6cbd10e16ab46cf23b3e4157e487789501493be4
ww-rm <ww-rm@qq.com>
2021-03-27 16:27:38 +0800

    fix post wrapper

--- wrapper.py
+++ wrapper.py
@@ -17,7 +17,7 @@ def browser_get(log_file=sys.stderr, max_sleep_seconds=1):
                 print(e.__class__, e, file=log_file)
             if not ret:
                 ret = None
-                print("Empty Return:{}:{}:{}".format(method.__name__, args, kwargs), file=log_file)
+                print("Empty Get:{}:{}:{}".format(method.__name__, args, kwargs), file=log_file)
             return ret
         return decorated_method
     return decorator
@@ -27,11 +27,15 @@ def browser_post(log_file=sys.stderr, max_sleep_seconds=1):
     def decorator(method):
         @wraps(method)
         def decorated_method(self, *args, **kwargs):
+            ret_code = False
             sleep(0.1+random.random()*(max_sleep_seconds-0.1))
             try:
-                method(self, *args, **kwargs)
+                ret_code = method(self, *args, **kwargs)
             except Exception as e:
                 print(e.__class__, e, file=log_file)
+            if not ret_code:
+                print("Failed Post:{}:{}:{}".format(method.__name__, args, kwargs), file=log_file)
+            return ret_code
         return decorated_method
     return decorator
 
@@ -41,11 +45,7 @@ def cookies_required():
     def decorator(method):
         @wraps(method)
         def decorated_method(self, *args, **kwargs):
-            if not (
-                self.session.cookies.get("PHPSESSID", domain=".pixiv.net", path="/") and
-                self.session.cookies.get("device_token", domain=".pixiv.net", path="/") and
-                self.session.cookies.get("privacy_policy_agreement", domain=".pixiv.net", path="/")
-            ):
+            if not self.session.cookies.get("PHPSESSID", domain=".pixiv.net", path="/"):
                 raise PermissionError("Cookies not found!")
             else:
                 return method(self, *args, **kwargs)
